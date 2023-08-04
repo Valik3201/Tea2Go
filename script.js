0ae850e03fd65a005a8a021c96f4a3679d478099
Valik <valik3201@gmail.com>
2023-08-04 10:55:06 +0200

    Init Tea2Go

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+document.getElementById("tasteButton").addEventListener("click", () => {
+  alert(`Great news! For the next 24 hours, you can enjoy an exclusive discount on our New Summer Blend by using the promo code "SUMMERJOY" at checkout. Don't miss out on this limited-time offer to savor the refreshing and fruity flavors of the season.
+
+Act fast and secure your taste of summer before the promo expires. We can't wait for you to experience the essence of summer in every delightful sip.
+  
+Thank you for choosing Tea2Go!
+  
+Tea2Go Team`);
+});
